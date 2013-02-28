7d3fde2597d0a4e3f7f7090b065bda200912c8c7
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-02-28 16:49:35 -0500

    json data

--- js/json.js
+++ js/json.js
@@ -5,3 +5,37 @@
  *  02-28-2013
  */
  //json dummy data for Timestomper
+ var json = {
+	 "contact1":{
+		 "group":["Group:", "End"],
+		 "sure":["Sure:", "6"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Drill"],
+		 "description":["Description:", "Fire drill"],
+		 "time":["Time:", "8:30"],
+		 "date":["Date:", "2/10/2013"]
+	 },
+	 {
+	 "contact2":{
+		 "group":["Group:", "Start"],
+		 "sure":["Sure:", "8"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Game"],
+		 "description":["Description:", "Chicago Bears"],
+		 "time":["Time:", "11:45"],
+		 "date":["Date:", "12/12/2012"]
+	 },
+	 {
+	 "contact3":{
+		 "group":["Group:", "Arrival"],
+		 "sure":["Sure:", "3"],
+		 "format":["Format:", "Standard"],
+		 "name":["Name:", "Airport"],
+		 "description":["Description:", "Chicago Midway"],
+		 "time":["Time:","2:15"],
+		 "date":["Date:","1/22/2013"]
+	 }
+
+	 
+	 
+ }
